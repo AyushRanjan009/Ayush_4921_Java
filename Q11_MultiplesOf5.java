69152a6ecec884edd41ecacb8d1d8ce2a88fcd5f
AyushRanjan009 <ayushranjan@AYUSHs-MacBook-Air-4.local>
2026-08-23 22:19:48 +0530

    Added Q10 to Q14 Java programs

---
+++ Q11_MultiplesOf5.java
@@ -0,0 +1,8 @@
+public class Q11_MultiplesOf5 {
+    public static void main(String[] args) {
+
+        for (int i = 5; i <= 100; i = i + 5) {
+            System.out.print(i + " ");
+        }
+    }
+}
